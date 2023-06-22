09fb1ae6efd43f462d7abbf9f16826b2b046f69d
Libre service <libreservice@local.agcdijonsud.com>
2023-06-22 13:07:36 +0200

    final social login project with facebook google github

---
+++ app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+
+class SocialAuthController extends Controller
+{
+    /*************************Facebook login *********************************/
+
+    // Click on the Login button, It will redirect to the Login page of the social network 
+    public function facebookRedirect() {
+        // return Socialite::driver('facebook')->setScopes([''])->redirect();  // default is scope=email,
+
+        // I have turned on the Email Permission, and now I can get email data
+        return Socialite::driver('facebook')->redirect();      
+    }
+
+    // if login is well done, we will be redirected to our page callback to do this treatment below
+    public function facebookLoginHandle() {
+        try {
+            $userData = Socialite::driver('facebook')->user();      // ->stateless(). Get user data from facebook
+            // dd($userData);
+            $socialNetwork = 'facebook';
+            $user = User::updateOrCreate(                   // update or create an user
+                ['facebook_id'   => $userData->id],         // condition for finding the instance
+                [                                           // if found, update with the 2nd array
+                    'name'      => $userData->name,         // if not found, create with both 1er and 2nd arrays
+                    'nickname'  => $userData->nickname ?? '',                 
+                    'email'     => $this->emailHandler($userData->email, $socialNetwork)    
+                ]
+            );
+
+            // dd($user);
+            Auth::login($user);                             // login to our app with 'user' instance
+            $redirectTo = RouteServiceProvider::HOME ?? '/home';
+            return redirect($redirectTo);                   // redirect to Homepage of User
+
+        } catch (\Exception $e) {
+            echo ($e->getMessage());                        // display the errors
+        }
+    }
+
+    /*************************Google login *********************************/
+
+    public function googleRedirect() {
+
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function googleLoginHandle() {
+        try {
+            $userData = Socialite::driver('google')->user();      // ->stateless()
+            
+            $socialNetwork = 'google';
+            $user = User::updateOrCreate(
+                ['google_id'   => $userData->id],           
+                [                                           
+                    'name'      => $userData->name,         
+                    'nickname'  => $userData->nickname ?? '',
+                    'email'     => $this->emailHandler($userData->email, $socialNetwork)    
+                ]
+            );
+            
+            Auth::login($user);
+            $redirectTo = RouteServiceProvider::HOME ?? '/home';
+            return redirect($redirectTo);
+
+        } catch (\Exception $e) {
+            echo ($e->getMessage());
+        }
+    }
+
+    /*************************Github login *********************************/
+
+    public function githubRedirect() {
+
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function githubLoginHandle() {
+        try {
+            $userData = Socialite::driver('github')->user();      // ->stateless()
+            
+            $socialNetwork = 'github';
+            $user = User::updateOrCreate(
+                ['github_id'   => $userData->id],           
+                [                                           
+                    'name'      => $userData->name ?? 'github_client_'.$userData->id,
+                    'nickname'  => $userData->nickname ?? '',
+                    'email'     => $this->emailHandler($userData->email, $socialNetwork)    
+                ]
+            );    
+            
+            Auth::login($user);
+            $redirectTo = RouteServiceProvider::HOME ?? '/home';
+            return redirect($redirectTo);
+
+        } catch (\Exception $e) {
+            echo ($e->getMessage());
+        }
+    }
+
+    /************************* Functions Helpers *****************************/
+
+    public function emailHandler(string $email, string $socialNetwork) : string
+    {
+        return "($socialNetwork)_$email";
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+
+use App\Http\Controllers\SocialAuthController;
+
+// use Laravel\Socialite\Two\FacebookProvider; // Day la file define scope=['email'] cua Socialite. Do vay luc nao cung co scope nay tren URL
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('/auth')->name('auth.')->middleware('guest')->group(function(){
+    Route::get('facebook', [SocialAuthController::class, 'facebookRedirect'])->name('facebook');
+    Route::get('facebook/callback', [SocialAuthController::class, 'facebookLoginHandle']);
+
+    Route::get('google', [SocialAuthController::class, 'googleRedirect'])->name('google');
+    Route::get('google/callback', [SocialAuthController::class, 'googleLoginHandle']);
+
+    Route::get('github', [SocialAuthController::class, 'githubRedirect'])->name('github');
+    Route::get('github/callback', [SocialAuthController::class, 'githubLoginHandle']);
+
+    Route::get('linkedin', [SocialAuthController::class, 'linkedinRedirect'])->name('linkedin');
+    Route::get('linkedin/callback', [SocialAuthController::class, 'linkedinLoginHandle']);
+});
